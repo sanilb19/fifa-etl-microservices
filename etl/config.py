eeb28a2f289e82f1eb91150f236f71cc093fbc52
Sanil Baweja <sanilbaweja@gmail.com>
2025-05-30 04:56:59 -0700

    commit deleted files

--- etl/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
-
-RAW_DATA_PATH = Path(__file__).resolve().parent.parent / "data/archive"
-DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING", "postgresql://postgres:postgres@localhost:5432/fifa")
